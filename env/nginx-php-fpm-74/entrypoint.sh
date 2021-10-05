377d455f04b8e9e198b76d2d6eb6400292472915
markhsieh4good <79235892+markhsieh4good@users.noreply.github.com>
2021-10-05 16:14:37 +0800

    Update entrypoint.sh

--- env/nginx-php-fpm-74/entrypoint.sh
+++ env/nginx-php-fpm-74/entrypoint.sh
@@ -11,4 +11,12 @@ if [ -e "/etc/nginx/site-enabled/default" ]; then
   rm /etc/nginx/site-enabled/default
 fi
 
+if [ -e "/etc/service/howtostart" ]; then
+  CMD=`cat /etc/service/howtostart`
+  if [ ! -e "/var/log/service.log" ]; then
+    touch /var/log/service.log
+  fi 
+  /bin/bash -c "cd /etc/service && $CMD" 2>&1 3>&1 >>/var/log/service.log &
+fi
+
 nginx -g "daemon off;"
