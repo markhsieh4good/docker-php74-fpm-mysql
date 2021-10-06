d6b1f712e1f3fb89c996a9429832f74df7c50352
mark.hsieh-macbook-pro <mark.hsieh@hwaotech.com>
2021-10-06 18:29:41 +0800

    php-fpm + php-cli + nginx + manual nginx.conf

--- env/nginx-php-fpm-74/entrypoint.sh
+++ env/nginx-php-fpm-74/entrypoint.sh
@@ -1,35 +1,71 @@
 #!/bin/bash
 
+_PWD=`pwd`
 echo -n "===== start bash == "
 date "+%Y/%m/%d %H:%M:%S"
 echo ""
 
+echo "====== clear old nginx setting ======"
 if [ -e "/etc/nginx/conf.d/default.conf" ]; then
   rm /etc/nginx/conf.d/default.conf
 fi
-if [ -e "/etc/nginx/site-enabled/default" ]; then
-  rm /etc/nginx/site-enabled/default
+if [ -e "/etc/nginx/sites-enabled/default" ]; then
+  rm /etc/nginx/sites-enabled/default
 fi
-rm /etc/nginx/site-enabled/*
+cd /etc/nginx/sites-enabled/
+for f in `ls -a ./`
+do
+  if [ "$f" == "." ]; then
+    echo ""
+  elif [ "$f" == ".." ]; then
+    echo ""
+  else
+    echo "rm $f"
+    rm "$f"
+  fi
+done
+cd "$_PWD"
+echo ""
 
+echo "====== php-cli support ======="
 if [ -e "/opt/service/howtostart" ]; then
   CMD=`cat /opt/service/howtostart | head -1`
-  /bin/bash -c "cd /opt/service && $CMD" &
+  /bin/bash -c "cd /opt/service && $CMD"
 fi
+echo ""
 
+echo "====== supervisor======"
+service supervisor stop
+sleep 1
+service supervisor start
+service supervisor status
+sleep 2
+echo ""
+ps aux | grep "php-fpm" | grep "process"
+echo ""
+
+echo "======= nginx setting ======="
 if [ -e "/etc/nginx/sites-available/default.conf" ]; then
-  ln -s /etc/nginx/sites-available/default.conf /etc/nginx/conf.d/default.conf
+  ln -sf /etc/nginx/sites-available/default.conf /etc/nginx/conf.d/default
 fi
 
-for f in `ls -a /etc/nginx/sites-available/`
+cd /etc/nginx/sites-available/
+for f in `ls -a ./`
 do
   _NAME=`echo "$f" | xargs basename`
   if [[ "$f" == "default.conf" ]]; then
     echo ""
+  elif [ "$f" == "." ]; then
+    echo ""
+  elif [ "$f" == ".." ]; then
+    echo ""
   else
-    ln -s "$f" /etc/nginx/sites-enabled/"$_NAME"
+    echo "ln -s /etc/nginx/sites-available/$f /etc/nginx/sites-enabled/$_NAME"
+    ln -s "/etc/nginx/sites-available/$f" /etc/nginx/sites-enabled/"$_NAME"
   fi
 done
+cd "$_PWD"
+echo ""
 
 echo "======== ready to start nginx ==========="
 ls -alh /etc/nginx/sites-enabled/
